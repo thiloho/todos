eaa163c9f42fc0ca822874fab3b819b354272ff6
thiloho <123883702+thiloho@users.noreply.github.com>
2023-12-24 17:29:44 +0100

    Update account deletion section

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -9,32 +9,38 @@
 
 <h1>Account</h1>
 
-<h2>User information</h2>
+<section class="flex flex-col gap-8" id="overview">
+	<h2>User information</h2>
 
-<ul class="flex flex-col gap-2">
-	<li>Provider: <strong>{data.authProvider}</strong></li>
-	<li>User id: <strong>{data.userId}</strong></li>
-	<li>Username: <strong>{data.username}</strong></li>
-</ul>
+	<ul class="flex flex-col gap-2">
+		<li>Provider: <strong>{data.authProvider}</strong></li>
+		<li>User id: <strong>{data.userId}</strong></li>
+		<li>Username: <strong>{data.username}</strong></li>
+	</ul>
 
-<form method="post" action="?/logout" use:enhance>
-	<button type="submit" class="btn">Log out</button>
-</form>
+	<form method="post" action="?/logout" use:enhance>
+		<button type="submit" class="btn">Log out</button>
+	</form>
+</section>
 
-<h2>User deletion</h2>
+<section class="flex flex-col gap-8" id="delete">
+	<h2>User deletion</h2>
 
-<details>
-	<summary class="btn max-w-fit cursor-pointer text-red-900 dark:text-red-400"
-		>Delete account</summary
-	>
-	<div class="flex flex-col gap-4 mt-4">
-		<Notification
-			type="danger"
-			text="<strong>Caution!</strong> If you delete your account, all of your account information will be
-			deleted. You can then create a new account, but your old information cannot be recovered."
-		/>
-		<form method="post" action="?/deleteAccount">
-			<button class="btn font-bold text-red-900 dark:text-red-400">Confirm account deletion</button>
-		</form>
-	</div>
-</details>
+	<details>
+		<summary class="btn max-w-fit cursor-pointer text-red-900 dark:text-red-400"
+			>Delete account</summary
+		>
+		<div class="flex flex-col gap-4 mt-4">
+			<Notification
+				type="danger"
+				text="<strong>Caution!</strong> If you delete your account, all of your account information will be
+				deleted. You can then create a new account, but your old information cannot be recovered."
+			/>
+			<form method="post" action="?/deleteAccount">
+				<button class="btn font-bold text-red-900 dark:text-red-400"
+					>Confirm account deletion</button
+				>
+			</form>
+		</div>
+	</details>
+</section>
